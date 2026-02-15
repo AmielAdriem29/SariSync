40cb2cbaed256f8d8081a3e4f729ce953a590a3e
ausdotsn50 <ausdotsn50@gmail.com>
2026-02-15 17:02:08 +0800

    feat: initial auth setup from Clerk

--- SariSync/app/_layout.tsx
+++ SariSync/app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(home)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ClerkProvider>
+  )
 }
