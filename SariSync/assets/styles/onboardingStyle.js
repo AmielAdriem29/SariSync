e78e52cc6ce642ac127d23a0c63c6f10a36f045d
ausdotsn50 <ausdotsn50@gmail.com>
2026-02-20 12:55:00 +0800

    styles: setup initial styles file and constant colors

---
+++ SariSync/assets/styles/onboardingStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "@/constants/color.js";
+
+export const styles = StyleSheet.create({
+  /*
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    padding: 20,
+    justifyContent: "center",
+  },
+  */
+  
+});
---
+++ SariSync/constants/color.js
@@ -0,0 +1,26 @@
+const sariSyncTheme = {
+    primary: "#E63946",
+    primaryReduc: "#FF8891",
+    text: "#6A6A6A",
+    textInv: "#FFFFFF",
+/*
+  primary: "#0277BD",
+  primReduc: "rgba(2, 119, 189, 0.5)",
+  background: "#E1F5FE",
+  text: "#01579B",
+  border: "#B3E5FC",
+  borderDrk: "rgba(12, 97, 137, 0.6)",
+  textLight: "#4FC3F7",
+  redShd: "#EF5350",
+  grnShd: "#26A69A",
+  card: "#FFFFFF",
+  shadow: "#000000",
+*/
+};
+
+export const THEMES = {
+  sariSync: sariSyncTheme,
+};
+
+// Theme switcher
+export const COLORS = THEMES.sariSync;
