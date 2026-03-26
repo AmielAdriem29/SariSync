d96de0823daab4b96c2b71b9d1ec410418607ffc
AmielAdriem29 <amieljpenaflor@gmail.com>
2026-03-26 18:45:58 +0800

    add POST /products route

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -5,12 +5,23 @@ const router = express.Router()
 
 router.get('/products', async (req,res) => {
   try {
-    const result = await pool.query('SELECT * from products')
+    const result = await pool.query(`SELECT * from products`)
     res.json(result.rows)
   } catch (error) {
-    res.status(500).json({ error: 'Error! products.ts'})
+    res.status(500).json({ error: `Error! abt getting products`})
   }
 })
 
+router.post('/products', async (req, res) => {
+  try {
+    const { name, barcode, category, cost_price, retail_price, current_stock, min_stock } = req.body;
+    const result = await pool.query(`INSERT INTO products (name, barcode, category, cost_price, retail_price, current_stock, min_stock, store_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *`, [name, barcode, category, cost_price, retail_price, current_stock, min_stock, 1]);
+    res.status(201).json(result.rows[0])
+    
+  } catch (error) {
+    res.status(500).json({ error: `Error! abt posting products`})
+  }
+})
 
 export default router
