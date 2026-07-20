7ff0907e7b0dcf85e981d8875e756992b2e5fcd8
ausdotsn50 <ausdotsn50@gmail.com>
2026-07-20 08:02:15 +0800

    fix: rounded green layout removal; column alignment in Flatlist (transactions page)

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,32 +1,38 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider, usePathname } from 'expo-router';
 import { useColorScheme, View, Text } from 'react-native';
 import NavigationTabs from '@/components/navigation-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors, IconSizes } from '@/constants/theme';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+const noHeaderRoutes = ['/record-transaction', '/manage-products', '/manage-credit', '/log-expense'];
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const colors = Colors[colorScheme === 'unspecified' ? 'light' : colorScheme];
+  const path = usePathname();
+  const showScreenHeader = !noHeaderRoutes.includes(path);
 
   return (
     <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <SafeAreaView edges={['top', 'bottom']} className="flex-1 bg-primary">
-        <View className="flex-row justify-between p-6">
-          <View className="flex-row items-center gap-2">
-            <View className="bg-white rounded-full self-center p-1.5">
-              <Ionicons name="person" size={IconSizes.medium} color={colors.neutral} />
+        { showScreenHeader &&
+          <View className="flex-row justify-between p-6">
+            <View className="flex-row items-center gap-2">
+              <View className="bg-white rounded-full self-center p-1.5">
+                <Ionicons name="person" size={IconSizes.medium} color={colors.neutral} />
+              </View>
+              <View>
+                <Text className="text-white font-bold text-sm">John Doe</Text>
+                <Text className="text-white text-xs">Owner</Text>
+              </View>
             </View>
-            <View>
-              <Text className="text-white font-bold text-sm">John Doe</Text>
-              <Text className="text-white text-xs">Owner</Text>
-            </View>
+
+            <Ionicons name="notifications" size={IconSizes.medium} color={colors.white} />
           </View>
+        }
 
-          <Ionicons name="notifications" size={IconSizes.medium} color={colors.white} />
-        </View>
-
-        <View className="flex-1 rounded-t-2xl overflow-hidden">
+        <View className={`flex-1 overflow-hidden ${showScreenHeader ? 'rounded-t-2xl' : ''}`}>
           <NavigationTabs />
         </View>
       </SafeAreaView>
